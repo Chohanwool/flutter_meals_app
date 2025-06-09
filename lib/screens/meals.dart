8e18bc2a74813fae3e2c3c9522058b47f256a48c
Chohanwool <josephchome888@gmail.com>
2025-06-09 21:47:56 +0900

    [feat] Loading Meals Data into Screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -10,9 +10,37 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh.. nothing here!!',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Text('data'),
+      body: content,
     );
   }
 }
