9f990177bd850dfcdeff0f1a35afa465bf91a90e
Chohanwool <josephchome888@gmail.com>
2025-06-09 20:56:55 +0900

    [feat] InkWell 위젯 사용(물결 효과)

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -11,22 +11,34 @@ class CategoryGridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Container widget gives options of background colors
-    return Container(
-      padding: const EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
+    return Material(
+      color: Colors.transparent,
+      borderRadius: BorderRadius.circular(16),
+      child: InkWell(
+        onTap: () {
+          print('object');
+        },
+        splashColor: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
         ),
       ),
     );
