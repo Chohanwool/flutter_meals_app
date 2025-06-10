d67e5fc99e06eccae74d698d2f2ba9ceaf16589b
Chohanwool <josephchome888@gmail.com>
2025-06-10 20:59:12 +0900

    [feat] added Side Drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -60,6 +61,7 @@ class _TablScreen extends State<TabsScreen> {
 
     return Scaffold(
       appBar: AppBar(title: Text(activePageTitle)),
+      drawer: const MainDrawer(),
       // body will set dynamically
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
